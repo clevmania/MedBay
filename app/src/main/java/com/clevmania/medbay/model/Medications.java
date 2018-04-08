7867f47271e2f8ede0f20125d2afca2273f93b68
lawrence <lawrence@grandilo.com>
2018-04-08 18:58:20 +0100

    created medications model

---
+++ app/src/main/java/com/clevmania/medbay/model/Medications.java
@@ -0,0 +1,74 @@
+package com.clevmania.medbay.model;
+
+/**
+ * Created by grandilo-lawrence on 4/8/18.
+ */
+
+public class Medications {
+    private String name;
+    private String desc;
+    private String interval;
+    private String start_date;
+    private String end_date;
+    private String dosage;
+
+    public Medications(String name, String desc, String interval, String start_date, String end_date, String dosage) {
+        this.name = name;
+        this.desc = desc;
+        this.interval = interval;
+        this.start_date = start_date;
+        this.end_date = end_date;
+        this.dosage = dosage;
+    }
+
+    public Medications() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getInterval() {
+        return interval;
+    }
+
+    public void setInterval(String interval) {
+        this.interval = interval;
+    }
+
+    public String getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(String start_date) {
+        this.start_date = start_date;
+    }
+
+    public String getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(String end_date) {
+        this.end_date = end_date;
+    }
+
+    public String getDosage() {
+        return dosage;
+    }
+
+    public void setDosage(String dosage) {
+        this.dosage = dosage;
+    }
+}
